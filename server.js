39eb1666ed3449bd336272b5841279128bbd6387
mono-praksa-projekt-admin <denz243010@gmail.com>
2025-04-09 16:36:17 +0200

    Ažurirane rute i Lightbox za Railway deployment

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const app = express();
+
+
+app.set('view engine', 'ejs');
 
 
 app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
-    res.send('Pozdrav sa Railway servera!');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
+app.get('/slike', (req, res) => {
+    const folderPath = path.join(__dirname, 'public', 'images');
+    const files = fs.readdirSync(folderPath);
+
+    const images = files
+        .filter(file => file.match(/\.(jpg|jpeg|png|svg)$/i))
+        .map((file, index) => ({
+            url: `/images/${file}`,
+            title: `Slika ${index + 1}`
+        }));
+
+    res.render('slike', { images });
 });
 
 
